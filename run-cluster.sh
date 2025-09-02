5d8b78496ddc46d2a7b9d8d048e946a1a3767510
Taardisaa <136665562@qq.com>
2025-09-02 14:36:37 -0600

    haoren ss

--- run-cluster.sh
+++ run-cluster.sh
@@ -43,12 +43,13 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOG_ROOT="${ROOT}/logs"
 
 function cluster_size() {
-    geni-get -a | \
-        grep -Po '<host name=\\".*?\"' | \
-        sed 's/<host name=\\"\(.*\)\\"/\1/' | \
-        sort | \
-        uniq | \
-        wc -l
+    # geni-get -a | \
+    #     grep -Po '<host name=\\".*?\"' | \
+    #     sed 's/<host name=\\"\(.*\)\\"/\1/' | \
+    #     sort | \
+    #     uniq | \
+    #     wc -l
+    echo 4
 }
 
 # Get available node count first to determine defaults
